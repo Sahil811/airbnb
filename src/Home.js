5fd06a5fb6ab72820079bd5bb0e47270dec2a8aa
unknown <sahilsiddiqui.dev@gmail.com>
2020-08-29 12:56:59 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import Banner from "./Banner.js";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Banner />
+    </div>
+  );
+};
+
+export default Home;
