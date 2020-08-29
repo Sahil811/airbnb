ff72de8bacb07e198a0aa495dde4dc8f1927d6e2
unknown <sahilsiddiqui.dev@gmail.com>
2020-08-29 14:18:22 +0530

    commit

--- src/Banner.js
+++ src/Banner.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search.js";
 
 const Banner = () => {
+  const [showSearch, setShowSearch] = useState(false);
   return (
     <div className="banner">
+      <div className="banner__search">
+        {showSearch && <Search />}
+        <Button
+          className="banner__searchButton"
+          variant="outlined"
+          onClick={() => setShowSearch(!showSearch)}
+        >
+          Search Dates
+        </Button>
+      </div>
+
       <div className="banner__info">
         <h1>Get out and stretch your imagination</h1>
 
