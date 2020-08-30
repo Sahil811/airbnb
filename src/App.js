a44abe33bc68a46e91b54c09a75073bda9837c1c
unknown <sahilsiddiqui.dev@gmail.com>
2020-08-30 12:00:57 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer";
+import SearchPage from "./SearchPage";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <BrowserRouter>
+        <Header />
+
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
